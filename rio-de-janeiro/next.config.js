ce4010738009183edccafa40b5f8814387530bb8
Bruno Nardini <megatroom3@gmail.com>
2022-04-28 12:33:21 -0300

    Add docker config

--- rio-de-janeiro/next.config.js
+++ rio-de-janeiro/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/riodejaneiro",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
